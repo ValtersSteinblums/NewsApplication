e47071ada85e9fd83fb8cf2f24a2f11cdaf59e88
ValtersSteinblums <109601588+ValtersSteinblums@users.noreply.github.com>
2022-12-12 10:21:38 +0200

    Initial Commit

---
+++ NewsApplication/NewsApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApplicationApp.swift
+//  NewsApplication
+//
+//  Created by valters.steinblums on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
